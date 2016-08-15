8b83cc01cbca47922ac5f11dca89f3420c6d8033
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2016-08-15 11:14:07 +1000

    Scaled overlays work as intended

--- AmazingSketch/ViewController.swift
+++ AmazingSketch/ViewController.swift
@@ -13,15 +13,17 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var editButto: UIButton!
+    @IBOutlet weak var workingImageView: UIImageView!
     
     var imageView: UIImageView!
     
     var lastPoint: CGPoint?
     var editting: Bool = false
+    var initialZoomScale: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         let image = UIImage(named: "highResImage", inBundle: nil, compatibleWithTraitCollection: nil)
         
         imageView = UIImageView(image: image)
@@ -30,8 +32,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         scrollView.contentSize = CGSize(width: (image?.size.width)!, height: (image?.size.height)!)
         
         scrollView.addSubview(imageView)
-    
-        let scaleFactor = view.frame.width / scrollView.contentSize.width
+        
+        let scaleFactor = (view.frame.height / scrollView.contentSize.height) < (view.frame.width / scrollView.contentSize.width) ? (view.frame.width / scrollView.contentSize.width) : (view.frame.height / scrollView.contentSize.height)
         
         scrollView.minimumZoomScale = scaleFactor
         scrollView.maximumZoomScale = 10
@@ -40,11 +42,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         scrollView.alwaysBounceHorizontal = false
         
         scrollView.zoomScale = scaleFactor
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        
+        initialZoomScale = scaleFactor
     }
     
     @IBAction func editHandler(sender: UIButton) {
@@ -54,6 +52,10 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         
         scrollView.userInteractionEnabled = !editing
         editButto.setTitle(title, forState: .Normal)
+        
+        if !editing {
+            commitChanges()
+        }
     }
     
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
@@ -62,28 +64,55 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
         
-        print("from: \(fromPoint) \n to: \(toPoint)\n")
+        let workingSize = workingImageView.frame.size
         
-        UIGraphicsBeginImageContext(view.frame.size)
+        UIGraphicsBeginImageContext(workingSize)
         
         let context = UIGraphicsGetCurrentContext()
-        self.imageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        self.workingImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: workingSize.width, height: workingSize.height))
         
-        let from = imageView.pointInAspectScaleFitImageCoordinates(fromPoint)
-        let to = imageView.pointInAspectScaleFitImageCoordinates(toPoint)
-        
-        CGContextMoveToPoint(context, from.x, from.y)
-        CGContextAddLineToPoint(context, to.x, to.y)
+        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
         
         CGContextSetLineCap(context, .Round)
         CGContextSetLineWidth(context, 2.0)
-        CGContextSetRGBStrokeColor(context, 1, 1, 1, 1.0)
+        CGContextSetRGBStrokeColor(context, 1, 1, 1, 1)
         CGContextSetBlendMode(context, .Normal)
         
         CGContextStrokePath(context)
         
+        self.workingImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    private func commitChanges() {
+        
+        let scrollViewZoomScale = scrollView.zoomScale - initialZoomScale! == 0 ? 1 : scrollView.zoomScale - initialZoomScale!
+        
+        let size = scrollView.contentSize
+        let workingSize = workingImageView.frame.size
+        
+        let offsetX = scrollView.contentOffset.x == 0 ? 1 : scrollView.contentOffset.x
+        let offsetY = scrollView.contentOffset.y == 0 ? 1 : scrollView.contentOffset.y
+        
+        let magicMultiplierX: CGFloat = 0.704
+        let magicMultiplierY: CGFloat = 0.978
+        
+        let scaleX = size.width / workingSize.width * magicMultiplierX
+        let scaleY = size.height / workingSize.height * magicMultiplierY
+        
+        let x = offsetX
+        let y = offsetY
+        let width = workingSize.width
+        let height = workingSize.height
+        
+        UIGraphicsBeginImageContext(size)
+        self.imageView.image?.drawInRect(CGRect(x: 0, y: 0, width: size.width, height: size.height), blendMode: .Normal, alpha: 1.0)
+        self.workingImageView.image?.drawInRect(CGRect(x: x, y: y, width: width, height: height), blendMode: .Normal, alpha: 1.0)
         self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
+        
+        self.workingImageView.image = nil
         
     }
     
@@ -101,30 +130,5 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         lastPoint = nil
-    }
-}
-
-extension UIImageView {
-    
-    func pointInAspectScaleFitImageCoordinates(point:CGPoint) -> CGPoint {
-        
-        if let img = image {
-            
-            let imageSize = img.size
-            let imageViewSize = frame.size
-            
-            let imgRatio = imageSize.width/imageSize.height // The ratio of the image before scaling.
-            let imgViewRatio = imageViewSize.width/imageViewSize.height // The ratio of the image view
-            
-            let ratio = (imgRatio > imgViewRatio) ? imageSize.width/imageViewSize.width:imageSize.height/imageViewSize.height // The ratio of the image before scaling to after scaling.
-            
-            let xOffset = (imageSize.width-(imageViewSize.width*ratio))*0.5 // The x-offset of the image on-screen (as it gets centered)
-            let yOffset = (imageSize.height-(imageViewSize.height*ratio))*0.5 // The y-offset of the image on-screen (as it gets centered)
-            
-            let subImgOrigin = CGPoint(x: point.x*ratio, y: point.y*ratio); // The origin of the image (relative to the origin of the view)
-            
-            return CGPoint(x: subImgOrigin.x+xOffset, y: subImgOrigin.y+yOffset);
-        }
-        return CGPointZero
     }
 }
